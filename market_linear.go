56c98e9bd4a82046024c3fdbcd061368b00a5fc1
Iago Quintero <iagoquintero00@gmail.com>
2021-06-05 22:20:16 -0300

    added market linear and fix go mod

--- market_linear.go
+++ market_linear.go
@@ -60,3 +60,26 @@ func (s *MarketService) LinearTickers(symbol SymbolUSDT) (*LinearTickersResponse
 	}
 	return &res, nil
 }
+
+// OrderBook :
+func (s *MarketService) LinearOrderBook(symbol SymbolUSDT) (*OrderBookResponse, error) {
+	var res OrderBookResponse
+
+	params := map[string]string{
+		"symbol": string(symbol),
+	}
+
+	url, err := s.Client.BuildPublicURL("/v2/public/orderBook/L2", params)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if err := json.NewDecoder(resp.Body).Decode(&res); err != nil {
+		return nil, err
+	}
+	return &res, nil
+}
